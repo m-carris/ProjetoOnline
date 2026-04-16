0e4067325759804b5b4f7e08f3869d87e6934c72
Miguel A. Serafim <MASerafim@carris.pt>
2026-04-16 11:20:36 +0100

    Added Carris Site

---
+++ EstruturaExemplo/js/google-analytics.init.js
@@ -0,0 +1,13 @@
+﻿(function (window) {
+
+    function gtag() { dataLayer.push(arguments); }
+
+    if (!window.dataLayer || !window.dataLayer.length) {
+
+        window.dataLayer = window.dataLayer || [];
+
+        gtag('js', new Date());
+
+        gtag('config', 'UA-9667621-1');
+    }
+})(window);
